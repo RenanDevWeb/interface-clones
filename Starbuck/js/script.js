9b310b64112faf053a23813a9bbb76539b860314
Renan <renansalmazio@gmail.com>
2021-12-26 18:39:43 -0300

    adicionando Starbuck

---
+++ Starbuck/js/script.js
@@ -0,0 +1,9 @@
+
+
+function changeImage(path){
+  document.querySelector('.starbucks').src = path
+}
+function changeColor(color){
+  const circle = document.querySelector('.circle');
+  circle.style.background = color; 
+}
